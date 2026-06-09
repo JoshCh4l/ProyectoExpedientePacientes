1f5d10c6b2fd75f5f0554b0a2e62d60f3be00613
CrazyRacoon <joshuachaconalvarez@gmail.com>
2026-06-09 04:32:27 -0600

    Arreglo-Migraciones

--- obj/Debug/net10.0/ProyectoExpedientePacientes.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProyectoExpedientePacientes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoExpedientePacientes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40330e6830930f551abd5a94b1dc7a8c1ab2790b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+230a4594dd4b563e6c71ba7e7d54689832031907")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoExpedientePacientes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoExpedientePacientes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
